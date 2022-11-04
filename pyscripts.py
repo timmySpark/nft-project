81d5ba00afddd89fa54dcce631762d72e6607d86
timmySpark <timmyspark1@gmail.com>
2022-11-04 16:12:02 +0000

    feat:Restructured pyscripts file

--- pyscripts.py
+++ pyscripts.py
@@ -1,126 +1,107 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+import os
 import csv
 import json
 import hashlib
 
 # collect Input for csv and json files
 
-csvinput = str(input('Enter csv file : '))
-jsoninput = csvinput.replace('.csv', '_output.json')
+csvinput = str(input('Enter csv file path : '))
+
+def welcome():
+    print('Welcome to Nft-Projects')
+    # Check if file is a csv file
+
+    with open(csvinput) as file:
+        if file.read(1) in '{[':
+            print('oops , seems like file isnt a csv file ,its likely JSON please try again')
+        else:
+            file.seek(0)
+            reader = csv.reader(file)
+            try:
+                if len(next(reader)) == len(next(reader)) > 1:
+                    print('This is a csvfile')
+            except StopIteration:
+                pass
 
 
 # func split_attrib: to split the attributes in the csv file
 
 def split_attrib(attr):
     i = 0
+    attrs = []
     new_attr = attr.split(';')
     for val in new_attr:
         splited = val.split(':')
-        return{
-            "trait_type":splited[0],
-            "value":splited[1]
-        }
-    '''
-
-    for (i, val) in enumerate(new_attr):
-        if i > 7:
-            break
-        else:
-            splited = val.split(':')
-
-            # print (i, ",",splited , ',' , splited[0],',',splited[1])
-
-            print ('trait_type', splited[0], ',', 'value', splited[1])
-            i += 1
-    '''
-
-# func(make_record): create CH-0007 Json Format
-
-def make_record(row):
-
-    # print(row['attributes'])
-
-    return {  # i want to get the length of all rows in the csv
-              # Attributes is in object but it's to
-        'format': 'CHIP-0007',
-        'name': row['Name'],
-        'description': row['Description'],
-        'minting_tool': row['TEAM NAMES'],
-        'sensitive_content': False,
-        'series_number': int(row['Series Number']),
-        'series_total': len(row),
-        'gender': row['Gender'],
-        'attributes': [split_attrib(row['attributes']),
-                       {'trait_type': '', 'value': ''}],
-        'collection': {'name': '', 'id': '', 'attributes': [{'type': ''
-                       ,
-                       'value': 'Rewards for accomplishments during HNGi9.'
-                       }]},
-        }
+        attrs.append({'trait_type': splited[0], 'value': splited[0]})
+    return attrs
 
 
-# func(hashedkey): creates hash keys for json files
+# func(hashedkey): creates hash keys for each json file
 
 def hashedkey(filename):
     hashvalue = hashlib.sha256(filename.encode())
     return hashvalue.hexdigest()
 
 
-# func(add_column_in_csv): with inputs from the fuction below ,
-# read csv collected as input and write in a new csv file
-# with Hash as an extra column
+# func(make_record): create CH-0007 Json Format
 
-def add_column_in_csv(inputfile, outputfile):
-    with open(inputfile, 'r') as read_csv:
-        with open(outputfile, 'w', newline='') as write_csv:
+def make_record(row):
+    return {
+        'format': 'CHIP-0007',
+        'name': row['Name'],
+        'description': row['Description'],
+        'minting_tool': row['TEAM NAMES'],
+        'sensitive_content': False,
+        'series_number': int(row['Series Number']),
+        'series_total': 420,
+        'gender': row['Gender'],
+        'attributes': [split_attrib(row['attributes'])],
+        'collection': {'name': 'Zuri NFT Tickets for Free Lunch',
+                       'id': 'b774f676-c1d5-422e-beed-00ef5510c64d',
+                       'attributes': [{'type': 'description',
+                       'value': 'Rewards for accomplishments during HNGi9.'
+                       }]},
+        }
 
-            # Create a csv.reader object from the input file object
+    print('CH-007 format successfully created')
 
-            csv_reader = csv.reader(read_csv)
+''' 
+    /* This function does the following :-
+    /* Create a folder 'jsonfiles' if file does not exist
+    /* Read csv file collected as input 
+    /* Create json files from each row in the csv
+    /* Create a new csv file with jsonfile hashedkey added as a new column
+        
+'''
 
-            # Create a csv.writer object from the output file object
+def run(inputfile, outputfile):
+    if not os.path.exists('jsonfiles'):
+        os.mkdir('jsonfiles')
 
-            csv_writer = csv.writer(write_csv)
+    with open(inputfile, 'r') as csvfile:
+        with open(outputfile, 'w') as write_csv:
+            reader = csv.DictReader(csvfile)
+            fields = reader.fieldnames + ['HASH_SHA256']
+            csv_writer = csv.DictWriter(write_csv, fields)
+            csv_writer.writeheader()
+            lcount = 0
 
-            count = 0
-            i = 0
-            for row in csv_reader:
-                if count == 0:
-                    row.append('Hash')
-                # else:
-                    # while i < len(hashes):
-                    #     row.append(hashes[i])
-
-                    # row.append(newcolumn)
+            for row in reader:
+                dump = make_record(row)
+                filename = 'jsonfiles/' + row['Filename'] + '.json'
+                newline = hashedkey(filename)
+                dump['Hash'] = newline
+                out = json.dumps(dump, indent=4)
+                jsonoutput = open(filename, 'w')
+                jsonoutput.write(out)
+                lcount += 1
+                jsonoutput.close()
+                row['HASH_SHA256'] = str(newline)
                 csv_writer.writerow(row)
-                count += 1
+
+    print('Operation completed Succesfully')
 
 
-# Read csv file collected as input ,
-# create json files from each row in the csv
-
-with open(csvinput, 'r') as csvfile:
-
-    reader = csv.DictReader(csvfile)
-
-    lcount = 0
-
-    for row in reader:
-
-        dump=make_record(row)
-        filename = row['Filename'] + '.json'
-        newline = hashedkey(filename)
-        dump['Hash'] = newline
-        out = json.dumps(dump, indent=4)
-        jsonoutput = open(filename, 'w')
-        jsonoutput.write(out)
-
-
-        lcount += 1
-
-        # print(newline)
-
-    add_column_in_csv(csvinput, 'output.csv')
-
-    jsonoutput.close()
+if __name__ == '__main__':
+    welcome()
+    run(csvinput, csvinput.replace('.csv', '.output.csv'))
